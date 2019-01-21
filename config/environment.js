9224b9cc97fa91d01462a088eaec9f4e7915c933
Simranjeet singh <42518706+Sjsingh101@users.noreply.github.com>
2019-01-21 16:19:22 +0530

    Text overflow issue fixed (#7)
    * Text overflow issue fixed
    
    * Update README.md
    
    * Refresh patched

--- config/environment.js
+++ config/environment.js
@@ -44,6 +44,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+      ENV.locationType = 'hash';
     // here you can enable a production-specific feature
   }
 
