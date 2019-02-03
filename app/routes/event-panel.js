10356c890868531464ed998dd231ec7fde657d91
Manish Devgan <manish.nsit8@gmail.com>
2019-02-03 20:36:58 +0530

    Fetching events

--- app/routes/event-panel.js
+++ app/routes/event-panel.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
+    model() {
+        return RSVP.hash({
+            events : this.get('store').findAll('events')
+      });
+    }
 });
