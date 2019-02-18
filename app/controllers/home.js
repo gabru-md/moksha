becbbc4aadd256bcc1baab94d6d811cd80723918
= <geniuspant@gmail.com>
2019-02-18 10:22:34 +0530

    WIP

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -5,7 +5,10 @@ export default Controller.extend({
     eventType : '',
     actions : {
         changeEventController(eventHandler) {
+<<<<<<< HEAD
             console.log(eventHandler);
+=======
+>>>>>>> 7eb7ae06a7d3600fb9741eb9596473b4ed4c827f
             this.set('displayEvent',eventHandler.target.value);
             this.set('eventType', eventHandler.target.type);
         }
