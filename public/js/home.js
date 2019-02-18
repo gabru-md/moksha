93ab94f3f874d652899a8fd7f9decafca9006363
= <geniuspant@gmail.com>
2019-02-18 10:11:26 +0530

    WIP

--- public/js/home.js
+++ public/js/home.js
@@ -52,8 +52,8 @@ var countDown = setInterval(function () {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  $('.countdown1').html(days + "d " + hours + "h " );
-  $('.countdown2').html( minutes + "m "+" " + seconds + "s");
+  $('.countdown1').html(days + "d " + hours + "h ");
+  $('.countdown2').html(minutes + "m " + " " + seconds + "s");
   if (distance < 0) {
     clearInterval(countDown);
     $('.countdown1').html('wait is over');
@@ -73,16 +73,16 @@ var flutter = setInterval(() => {
   }
 }, 300);
 
-function on_event_data(){
+function on_event_data() {
   off_overlay("overlay_events");
   on_overlay("super_overlay_events");
   document.getElementById("navcss2").style.display = "flex";
 }
 
-function off_event_data(){
-  off_overlay("super_overlay_events");
-  off_overlay("navcss2");
-}
+// function off_event_data(){
+//   off_overlay("super_overlay_events");
+//   off_overlay("navcss2");
+// }
 
 function on_events() {
   blurit();
@@ -92,6 +92,7 @@ function on_events() {
     on_overlay("overlay_events");
     applyprop("_events");
     off_overlay("super_overlay_events");
+    off_overlay("back");
   }, 300);
   hideAllInitially();
 }
@@ -108,6 +109,7 @@ function on_contacts() {
     off_events();
     off_overlay("super_overlay_events");
     // off_event_data();
+    off_overlay("back");
     on_overlay("overlay_contacts");
     applyprop("_contact");
   }, 300);
@@ -124,6 +126,7 @@ function on_home() {
   off_contacts();
   off_overlay("super_overlay_events");
   // off_event_data();
+  off_overlay("back");
   applyprop("_home");
   // document.getElementById("_home").className = ".blur";
 
@@ -140,26 +143,33 @@ let unblurit = () => {
   document.getElementById("bg-img").classList.remove("blur");
 }
 
-let  hideAllEventsExcept = (id) => {
-  var arr = ["_pronite","_music","_dance","_theatre","_literary","_gaming","_talent","_informal","_automobile"];
+let hideAllEventsExcept = (id) => {
+  var arr = ["_pronite", "_music", "_dance", "_theatre", "_literary", "_gaming", "_talent", "_informal", "_automobile"];
+  off_overlay("back");
   arr.forEach(element => {
-    if(element === id) {
-      document.getElementById(element).style.display="block";
-    }else{
-    document.getElementById(element).style.display="none";
+    if (element === id) {
+      document.getElementById(element).style.display = "block";
+      on_overlay("back");
+    } else {
+      document.getElementById(element).style.display = "none";
     }
   });
 }
 let hideAllInitially = () => {
-  var arr = ["_pronite","_music","_dance","_theatre","_literary","_gaming","_talent","_informal","_automobile"];
+  var arr = ["_pronite", "_music", "_dance", "_theatre", "_literary", "_gaming", "_talent", "_informal", "_automobile"];
   arr.forEach(element => {
-    document.getElementById(element).style.display="none";
+    document.getElementById(element).style.display = "none";
   });
 }
 
-on_events_detail = ()=>{
+on_events_detail = () => {
   off_overlay("overlay_events");
   on_overlay("super_overlay_events");
+  document.getElementById("data").classList.remove("d-sm-none");
+  document.getElementById("data").classList.add("d-none");
+  document.getElementById("scroll").classList.remove("d-none");
+  document.getElementById("scroll").classList.add("d-sm-none");
+  off_overlay("back");
 }
 
 function changeView(element) {
@@ -167,9 +177,26 @@ function changeView(element) {
   // showNav2();
   on_events_detail();
   hideAllEventsExcept(element.value);
+  document.getElementById("scroll").classList.remove("d-sm-none");
+  document.getElementById("scroll").classList.add("d-none");
+  document.getElementById("data").classList.remove("d-none");
+  document.getElementById("data").classList.add("d-sm-none");
 }
 
+back = () => {
+  document.getElementById("scroll").classList.remove("d-sm-none");
+  document.getElementById("scroll").classList.add("d-none");
+  document.getElementById("data").classList.remove("d-none");
+  document.getElementById("data").classList.add("d-sm-none");
+  off_overlay("back");
+}
+
+change = (element) => {
+  changeView(element);
+  off_overlay("back");
+}
 
 // initial
 
 hideAllInitially();
+off_overlay("back");
