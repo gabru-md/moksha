fc1653d9164e24d2b33a9e55447cef83e20de2d9
Manish Devgan <manish.nsit8@gmail.com>
2019-02-20 13:40:58 +0530

    Imporves .build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,6 +6,9 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     sassOptions: {
       extension: 'scss'
+    },
+    fingerprint: {
+      exclude: ['images/']
     }
   });
 
