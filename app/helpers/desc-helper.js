14241a841195e4a23fca87157fb9d98369b8794e
Manish Devgan <manish.nsit8@gmail.com>
2019-03-02 23:52:22 +0530

    pre-moksha changes

--- app/helpers/desc-helper.js
+++ app/helpers/desc-helper.js
@@ -4,7 +4,16 @@ import { htmlSafe } from '@ember/string';
 export function descHelper(params/*, hash*/) {
   var desc = params[0];
   desc = desc.replace(/(\r\n|\n|\r)/gm, '<br>');
-  return htmlSafe(desc);
+
+  function urlify(text) {
+    var urlRegex = /(https?:\/\/[^\s]+)/g;
+    return text.replace(urlRegex, function(url) {
+        return '<a href="' + url + '" target="_blank" style="font-weight: bold; color:white; text-decoration-color: white">' + url + '</a>';
+    })
+    // or alternatively
+    // return text.replace(urlRegex, '<a href="$1">$1</a>')
+  }
+  return htmlSafe(urlify(desc));
 }
 
 export default helper(descHelper);
