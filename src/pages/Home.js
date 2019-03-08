ca538b6aaf03c6c6c2325144f8f78c67c1085c3e
Lakshay Mehra <lakshay344@gmail.com>
2019-03-08 22:44:14 +0530

    Added blinking image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,33 @@ import React, { Component } from "react";
 import "./Home.css";
 
 class Home extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { showImg: false }
+
+    this.func = this.func.bind(this);
+  }
+
+  func() {
+    this.setState({ showImg: !this.state.showImg });
+  }
+
+  componentDidMount() {
+    setInterval(this.func, 600);
+  }
+
+  renderImg = () => {
+    if (this.state.showImg)
+      return <img id="logo1" className="img-fluid mx-auto" src="/images/-1.png" alt=""/>;
+  }
+
   render() {
     return (
       <div id="jumbotron">
         <img id="logo1" className="img-fluid mx-auto" src="/images/1.png" alt=""/>
+        {this.renderImg()}
         {/* <MokshaCountdown /> */}
         <img id="logo3" className="img-fluid mx-auto" src="/images/8.png" alt="Est 1983"/>
       </div>
